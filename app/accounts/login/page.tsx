24cd55ab83454c7ecd775807cf4f55dc2d1bf284
parastoomashkani <mashkaniparastoo@gmail.com>
2024-01-06 01:34:37 -0800

    add

--- app/accounts/login/page.tsx
+++ app/accounts/login/page.tsx
@@ -1,53 +1,108 @@
 "use client"
-
 import { useState } from 'react';
 import axios from 'axios';
 import Link from 'next/link';
+import Logo from "../../../public/images/logoNew (1).png";
+import Image from 'next/image';
 const Login = () => {
   const [mobile, setMobile] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
+  const sendVerificationCode = async () => {
+    try {
+      // Implement the logic to send the verification code
+      console.log('Sending verification code...');
+    } catch (err) {
+      console.error('Failed to send verification code:', err);
+    }
+  };
+
   const handleLogin = async () => {
     try {
-      const response = await axios.post('http://127.0.0.1:8000/api/login', {
+      // Check if the user has an account
+      const response = await axios.post('http://127.0.0.1:8000/api/check-account', {
         mobile,
-        password,
       });
 
-      const token = response.data.token;
+      const accountStatus = response.data.status;
 
-  
-      localStorage.setItem('Token',"Bearer "+token);
+      if (accountStatus === 200) {
+        // User has an account, check for further verification
+        await sendVerificationCode();
+      } else if (accountStatus === 220) {
+        // User has an account, no additional verification needed
+        const loginResponse = await axios.post('http://127.0.0.1:8000/api/login', {
+          mobile,
+          password,
+        });
 
-  
-      console.log( response.data.token. status);
+        const token = loginResponse.data.token;
+
+        localStorage.setItem('Token', 'Bearer ' + token);
+
+        console.log(loginResponse.data.token.status);
+      } else {
+        // router.push('/accounts/register');
+      }
     } catch (err) {
-
       setError('Invalid credentials. Please try again.');
       console.error('Login failed:', err);
     }
   };
 
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className='flex min-h-full flex-col justify-center px-6 py-12 lg:px-8'>
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <Image
+          src={Logo}
+          height={100}
+          width={100}
+          className="mx-auto h-10 w-auto"
+          alt='logo'
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          به حساب خود وارد شوید
+        </h2>
+        </div>
+  <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <div>
-        <label>Mobile:</label>
-        <input type="text" value={mobile} onChange={(e) => setMobile(e.target.value)} />
+        <label  className="block text-sm font-medium leading-6 text-gray-900">
+          موبایل :
+        </label>
+        <div className="mt-2">
+        <input type="text" value={mobile} onChange={(e) => setMobile(e.target.value)} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
       </div>
+      </div>
+
       <div>
-        <label>Password:</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </div>
+      <div className="flex items-center justify-between">
+        <label  className="block text-sm font-medium leading-6 text-gray-900">
+        رمز عبور :
+        </label>
+        </div>
+        <div className="mt-2">
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+     </div> </div>
       {error && <p style={{ color: 'red' }}>{error}</p>}
-      <button onClick={handleLogin}>Login</button>
-      <div>
-        <Link href="/accounts/register" >
-        <h3>
+      <div className="mb-12 pb-1 pt-1 text-center">
+      <button onClick={handleLogin} className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        Login
+        </button>
+        </div>
+        <div className="flex items-center justify-between pb-6">
+                    <p className="mb-0 mr-2">Don't have an account?</p>
+        <Link href="/accounts/register"   className="text-danger transition duration-150 ease-in-out hover:text-danger-600 focus:text-danger-600 active:text-danger-700">
+        <button
+                      type="button"
+                      className="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+                      data-te-ripple-init
+                      data-te-ripple-color="light">
           register
-        </h3></Link>
+          </button>
+     </Link>
       </div>
+    </div>
     </div>
   );
 };
--- app/accounts/register/page.tsx
+++ app/accounts/register/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 import { useState } from 'react';
 import axios from 'axios';
-
+import Logo from "../../../public/images/logoNew (1).png"
+import Image from 'next/image';
 const Register = () => {
   const [name, setName] = useState('');
   const [mobile, setMobile] = useState('');
@@ -21,7 +22,7 @@ const Register = () => {
       localStorage.setItem('token', 'Bearer ' + token);
       setIsRegistered(true);
     } catch (err) {
-      setError('Registration failed. Please try again.');
+      setError('.ثبت نام انجام نشد لطفا دوباره تلاش کنید');
       console.error('Registration failed:', err);
     }
   };
@@ -50,26 +51,45 @@ const Register = () => {
       localStorage.setItem('token', 'Bearer ' + token);
       console.log('Token saved to localStorage:', 'Bearer ' + token);
     } catch (err) {
+      setError ("کد درست نیست ");
       console.error('Code submission failed:', err);
     }
   };
   return (
-    <div>
-      <h1>Registration Page</h1>
+    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+       <Image
+   src={Logo}
+   height={100}
+   width={100}
+   className="mx-auto h-10 w-auto"
+   alt='logo'
+    />
+    
+    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+      <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        ایجاد حساب کاربری 
+      </h1>
       <div>
-        <label>Name:</label>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          نام 
+        </label>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
       </div>
       <div>
-        <label>Mobile:</label>
-        <input type="text" value={mobile} onChange={(e) => setMobile(e.target.value)} />
+        <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          موبایل 
+        </label>
+        <input type="text" value={mobile} onChange={(e) => setMobile(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
       </div>
       <div>
-        <label>Password:</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" >
+          رمز عبور
+        </label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
       </div>
       {error && <p style={{ color: 'red' }}>{error}</p>}
-      <button onClick={handleRegister}>Register</button>
+      <button onClick={handleRegister}  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" >Register</button>
 
       {isRegistered && (
         <div>
@@ -77,6 +97,8 @@ const Register = () => {
           <button onClick={handleSubmit}>Submit</button>
         </div>
       )}
+    </div>
+    </div>
     </div>
   );
 };
