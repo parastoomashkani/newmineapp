95d1208e0ee9c095c9aa1040de9ca9c75615d915
parastoomashkani <mashkaniparastoo@gmail.com>
2024-01-05 21:27:58 -0800

    add

--- app/pages/recruitment/page.tsx
+++ app/pages/recruitment/page.tsx
@@ -6,16 +6,12 @@ const page = () => {
     <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 lg:py-20">
         <div className="mb-4">
             <div className="mb-6 max-w-3xl text-center sm:text-center md:mx-auto md:mb-12">
-                <p className="text-base font-semibold uppercase tracking-wide text-blue-600 dark:text-blue-200">
-                    Contact
-                </p>
+              
                 <h2
                     className="font-heading mb-4 font-bold tracking-tight text-gray-900 dark:text-white text-3xl sm:text-5xl">
-                    Get in Touch
+            در تماس باشید
                 </h2>
-                <p className="mx-auto mt-4 max-w-3xl text-xl text-gray-600 dark:text-slate-400">In hac habitasse platea
-                    dictumst
-                </p>
+           
             </div>
         </div>
         <div className="flex items-stretch justify-center">
@@ -85,21 +81,46 @@ const page = () => {
                 <div className="card h-fit max-w-6xl p-5 md:p-12" id="form">
                     <h2 className="mb-4 text-2xl font-bold">Ready to Get Started?</h2>
                     <form id="contactForm">
-                        <div className="mb-6">
-                            <div className="mx-0 mb-1 sm:mb-4">
-                                <div className="mx-0 mb-1 sm:mb-4">
-                                    <label htmlFor="name" className="pb-1 text-xs uppercase tracking-wider"></label><input type="text" id="name" autocomplete="given-name" placeholder="Your name" class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0" name="name"/>
-                                </div>
-                                <div class="mx-0 mb-1 sm:mb-4">
-                                    <label for="email" class="pb-1 text-xs uppercase tracking-wider"></label><input type="email" id="email" autocomplete="email" placeholder="Your email address" class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0" name="email">
-                                </div>
-                            </div>
-                            <div class="mx-0 mb-1 sm:mb-4">
-                                <label for="textarea" class="pb-1 text-xs uppercase tracking-wider"></label><textarea id="textarea" name="textarea" cols="30" rows="5" placeholder="Write your message..." class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"></textarea>
-                            </div>
-                        </div>
-                        <div class="text-center">
-                            <button type="submit" class="w-full bg-blue-800 text-white px-6 py-3 font-xl rounded-md sm:mb-0">Send Message</button>
+                    <div className="mb-6">
+      <div className="mx-0 mb-1 sm:mb-4">
+        <div className="mx-0 mb-1 sm:mb-4">
+          <label htmlFor="name" className="pb-1 text-xs uppercase tracking-wider"></label>
+          <input
+            type="text"
+            id="name"
+            autoComplete="given-name"
+            placeholder="Your name"
+            className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
+            name="name"
+          
+          />
+        </div>
+        <div className="mx-0 mb-1 sm:mb-4">
+          <label htmlFor="email" className="pb-1 text-xs uppercase tracking-wider"></label>
+          <input
+            type="email"
+            id="email"
+            autoComplete="email"
+            placeholder="Your email address"
+            className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
+            name="email"
+          />
+        </div>
+      </div>
+      <div className="mx-0 mb-1 sm:mb-4">
+        <label htmlFor="textarea" className="pb-1 text-xs uppercase tracking-wider"></label>
+        <textarea
+          id="textarea"
+          name="textarea"
+          cols={30}
+          rows={5}
+          placeholder="Write your message..."
+          className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
+        ></textarea>
+      </div>
+    </div>
+                        <div className="text-center">
+                            <button type="submit" className="w-full bg-blue-800 text-white px-6 py-3 font-xl rounded-md sm:mb-0">Send Message</button>
                         </div>
                     </form>
                 </div>
