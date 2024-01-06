2ffb04cf386c0b49360cd049f6160735c72a415c
parastoomashkani <mashkaniparastoo@gmail.com>
2024-01-05 21:00:13 -0800

    add

--- app/accounts/sing/app/register/page.tsx
+++ app/accounts/sing/app/register/page.tsx
@@ -7,7 +7,7 @@ export default async function Register() {
   const session = await getServerSession();
   // const session = await getServerSession(authOptions);
 
-  if (session) redirect("/dashboard");
+  if (session) redirect("/");
 
   return <RegisterForm />;
 }
