bdbaa30acfb7726892c60897ff8f4c3931310dbd
parastoomashkani <mashkaniparastoo@gmail.com>
2024-01-05 23:10:10 -0800

    add 123456

--- app/accounts/register/page.tsx
+++ app/accounts/register/page.tsx
@@ -7,7 +7,8 @@ const Register = () => {
   const [mobile, setMobile] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-
+  const [isRegistered, setIsRegistered] = useState(false);
+  const [code, setCode] = useState('');
   const handleRegister = async () => {
     try {
       const response = await axios.post('http://127.0.0.1:8000/api/register', {
@@ -15,17 +16,29 @@ const Register = () => {
         mobile,
         password,
       });
-
       const token = response.data.token;
-      localStorage.setItem('token',"Bearer "+token);
-      console.log( response.data.token. status);
+      localStorage.setItem('token', 'Bearer ' + token);
+      setIsRegistered(true);
     } catch (err) {
       setError('Registration failed. Please try again.');
       console.error('Registration failed:', err);
     }
   };
+
+  const handleCodeChange = (event:any) => {
+    setCode(event.target.value);
+  };
   return (
     <div>
+       <div>
+  
+      {isRegistered && (
+        <div>
+          <input type="text" value={code} onChange={handleCodeChange} placeholder="Enter your code" />
+          <button>Submit</button>
+        </div>
+      )}
+    </div>
       <h1>Registration Page</h1>
       <div>
         <label>Name:</label>
@@ -41,6 +54,7 @@ const Register = () => {
       </div>
       {error && <p style={{ color: 'red' }}>{error}</p>}
       <button onClick={handleRegister}>Register</button>
+ 
     </div>
   );
 };
