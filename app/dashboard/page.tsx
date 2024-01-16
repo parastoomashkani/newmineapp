175c890a23dc27a20fbefde9d6067a0541e8c6c5
parastoomashkani <mashkaniparastoo@gmail.com>
2024-01-16 00:51:32 -0800

    add

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -148,7 +148,10 @@ className=''
                         <path className="fill-current text-gray-600 group-hover:text-cyan-600" d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
                         <path className="fill-current text-gray-300 group-hover:text-cyan-300" d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
                     </svg>
-                    <span className="group-hover:text-gray-700">Other data</span>
+                <Link href='/dashboard/chatBot'  >
+                    <span className="group-hover:text-gray-700">
+                        تیکت پشتیبانی 
+                        </span></Link>
                 </a>
             </li>
             <li>
